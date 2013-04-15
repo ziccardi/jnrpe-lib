df19f0217969db9100564b7d8bb16bd01c0b2aa2
ziccardi <massimiliano.ziccardi@gmail.com>
2013-04-15 16:03:08 +0200

    Added a new exception in case of plugin configuration error

---
+++ jnrpe-parent/jnrpe-lib/src/main/java/it/jnrpe/plugins/PluginConfigurationException.java
@@ -0,0 +1,26 @@
+package it.jnrpe.plugins;
+
+public class PluginConfigurationException extends Exception
+{
+
+    public PluginConfigurationException()
+    {
+        super();
+    }
+
+    public PluginConfigurationException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+
+    public PluginConfigurationException(String message)
+    {
+        super(message);
+    }
+
+    public PluginConfigurationException(Throwable cause)
+    {
+        super(cause);
+    }
+
+}
