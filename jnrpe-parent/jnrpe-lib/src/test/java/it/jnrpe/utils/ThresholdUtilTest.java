0b2e35c379e709e42e01a78aeb5e8b4b42f676ae
ziccardi <massimiliano.ziccardi@gmail.com>
2013-04-23 14:40:08 +0200

    Added tests for the StringUtils and the ThresholdUtil classes

---
+++ jnrpe-parent/jnrpe-lib/src/test/java/it/jnrpe/utils/ThresholdUtilTest.java
@@ -0,0 +1,62 @@
+package it.jnrpe.utils;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ThresholdUtilTest
+{
+    @Test
+    public void testSimpleThreshold() throws BadThresholdException
+    {
+        Assert.assertTrue(ThresholdUtil.isValueInRange("10", 5));
+        Assert.assertTrue(ThresholdUtil.isValueInRange("10", 0));
+        Assert.assertTrue(ThresholdUtil.isValueInRange("10", 10));
+        Assert.assertFalse(ThresholdUtil.isValueInRange("10", -1));
+        Assert.assertFalse(ThresholdUtil.isValueInRange("10", 11));
+        
+    }
+
+    @Test
+    public void testToInfinity() throws BadThresholdException
+    {
+        Assert.assertTrue(ThresholdUtil.isValueInRange("10:", 10));
+        Assert.assertTrue(ThresholdUtil.isValueInRange("10:", 100));
+        Assert.assertTrue(ThresholdUtil.isValueInRange("10:", 1000));
+        Assert.assertFalse(ThresholdUtil.isValueInRange("10:", 9));
+        Assert.assertFalse(ThresholdUtil.isValueInRange("10:", -11));
+    }
+
+    @Test
+    public void testNegativeInfinity() throws BadThresholdException
+    {
+        Assert.assertTrue(ThresholdUtil.isValueInRange("~:10", 10));
+        Assert.assertTrue(ThresholdUtil.isValueInRange("~:10", -10));
+        Assert.assertTrue(ThresholdUtil.isValueInRange("~:10", -100));
+        Assert.assertTrue(ThresholdUtil.isValueInRange("~:10", -1000));
+        Assert.assertFalse(ThresholdUtil.isValueInRange("~:10", 11));
+        Assert.assertFalse(ThresholdUtil.isValueInRange("~:10", 100));
+        Assert.assertFalse(ThresholdUtil.isValueInRange("~:10", 1000));
+    }
+ 
+    @Test
+    public void testBounded() throws BadThresholdException
+    {
+        Assert.assertTrue(ThresholdUtil.isValueInRange("10:20", 10));
+        Assert.assertTrue(ThresholdUtil.isValueInRange("10:20", 15));
+        Assert.assertTrue(ThresholdUtil.isValueInRange("10:20", 20));
+        Assert.assertFalse(ThresholdUtil.isValueInRange("10:20", 9));
+        Assert.assertFalse(ThresholdUtil.isValueInRange("10:20", 21));
+        Assert.assertFalse(ThresholdUtil.isValueInRange("10:20", -10));
+    }
+    
+    @Test
+    public void testNegatedBounded() throws BadThresholdException
+    {
+        Assert.assertFalse(ThresholdUtil.isValueInRange("@10:20", 10));
+        Assert.assertFalse(ThresholdUtil.isValueInRange("@10:20", 15));
+        Assert.assertFalse(ThresholdUtil.isValueInRange("@10:20", 20));
+        Assert.assertTrue(ThresholdUtil.isValueInRange("@10:20", 9));
+        Assert.assertTrue(ThresholdUtil.isValueInRange("@10:20", 21));
+        Assert.assertTrue(ThresholdUtil.isValueInRange("@10:20", -10));
+    }
+}
