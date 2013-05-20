36cdf16dbf391e831ce79cbb8d9edb7519501061
ziccardi <massimiliano.ziccardi@gmail.com>
2013-05-20 17:48:38 +0200

    Added a test for checktomcat

--- jnrpe-parent/jnrpe-plugins/src/main/java/it/jnrpe/plugin/CheckTomcat.java
+++ jnrpe-parent/jnrpe-plugins/src/main/java/it/jnrpe/plugin/CheckTomcat.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (c) 2013 Massimiliano Ziccardi
- *  
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at 
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0 
- * 
- * Unless required by applicable law or agreed to in writing, software 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 package it.jnrpe.plugin;
@@ -36,6 +36,7 @@ import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
+import org.apache.commons.codec.binary.Base64;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -43,174 +44,241 @@ import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 /**
- * The CheckTomcat plugin
- * This plugin does a HTTP GET of the tomcat status page, by default: /manager/status?XML=true
- * 
+ * The CheckTomcat plugin. This plugin does a HTTP GET of the tomcat status
+ * page, by default: /manager/status?XML=true
+ *
  * @author Frederico Campos
  *
  */
 public class CheckTomcat extends PluginBase {
 
-	public CheckTomcat(){
-		
-	}
-	/**
-	 * default values
-	 */
-	public final String DEFAULT_PORT = "8080";
-	public final String DEFAULT_URI = "/manager/status?XML=true";
-	public final int DEFAULT_TIMEOUT = 10;
-	
-	/* (non-Javadoc)
-	 * @see it.jnrpe.plugins.IPluginInterface#execute(it.jnrpe.ICommandLine)
-	 */
-	public ReturnValue execute(ICommandLine cl) throws BadThresholdException {
-		String username = cl.getOptionValue("username");
-		String password = cl.getOptionValue("password");
-		String hostname = cl.getOptionValue("hostname");
-		String port = cl.getOptionValue("port") != null ? cl.getOptionValue("port") : DEFAULT_PORT;
-		String uri =  cl.getOptionValue("uri") != null ? cl.getOptionValue("port") : DEFAULT_URI;
-		String warning = cl.getOptionValue("warning");
-		String critical = cl.getOptionValue("critical");
-		int timeout = cl.getOptionValue("timeout") != null ? Integer.parseInt(cl.getOptionValue("timeout")) : DEFAULT_TIMEOUT;
-		
-		if (!uri.startsWith("/")){
-			uri = "/" + uri;
-		}
-		String url = (cl.getOptionValue("ssl") != null ? "https://" :"http://") 
-				+ username + ":" + password + "@" + hostname + ":" + port + uri;
-		String encoded = new sun.misc.BASE64Encoder().encode((username+ ":" + password).getBytes());
-		Properties props = new Properties();
-		props.put("Authorization", "Basic " + encoded);
-		String response = null;
-		String errmsg = null;
-		try {
-			response = Utils.getUrl(new URL(url), props, timeout * 1000);
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-			errmsg = e.getMessage();
-		} catch (IOException e) {
-			e.printStackTrace();
-			errmsg = e.getMessage();
-		}
-		
-		if (response == null){
-			 return new ReturnValue(Status.WARNING, errmsg); 
-		}
-				
-		return analyseStatus(response, warning, critical);
-	}
-	
-	/**
-	 * Parse xml data and return status
-	 * @param xml
-	 * @param warning
-	 * @param critical
-	 * @return
-	 * ReturnValue
-	 */
-	private ReturnValue analyseStatus(String xml, String warning, String critical) {
-		StringBuffer buff = new StringBuffer();
-		System.out.println(xml);
-		String[] warn = warning != null ? warning.split(",") : null;
-		String[] crit = critical != null ? critical.split(",") : null;
-		
-		
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    /**
+     * Default Tomcat http port.
+     */
+    public static final String DEFAULT_PORT = "8080";
+
+    /**
+     * Default Tomcat manager URL.
+     */
+    public static final String DEFAULT_URI = "/manager/status?XML=true";
+
+    /**
+     * Default timeout.
+     */
+    public static final String DEFAULT_TIMEOUT = "10";
+
+    /**
+     * Executes the check.
+     *
+     * @param cl
+     *            The command line
+     * @return the result of the check
+     * @throws BadThresholdException
+     *             if the threshold could not be parsed
+     */
+    public final ReturnValue execute(final ICommandLine cl)
+            throws BadThresholdException {
+        String username = cl.getOptionValue("username");
+        String password = cl.getOptionValue("password");
+        String hostname = cl.getOptionValue("hostname");
+
+        String port = cl.getOptionValue("port", DEFAULT_PORT);
+        String uri = cl.getOptionValue("uri", DEFAULT_URI);
+        String warning = cl.getOptionValue("warning");
+        String critical = cl.getOptionValue("critical");
+
+        int timeout =
+                Integer.parseInt(cl.getOptionValue("timeout", DEFAULT_TIMEOUT));
+
+        // http(s)://user[:pwd]@hostname:port/uri
+        //String urlPattern = "{0}://{1}@{2}:{3}{4}";
+
+        if (!uri.startsWith("/")) {
+            uri = "/" + uri;
+        }
+
+        String protocol;
+        String credentials;
+
+        if (cl.hasOption("ssl")) {
+            protocol = "https://";
+        } else {
+            protocol = "http://";
+        }
+
+        if (password != null) {
+            credentials = username + ":" + password;
+        } else {
+            credentials = username + ":";
+        }
+
+        String url = protocol + credentials + "@" + hostname + ":" + port + uri;
+
+        // String url =
+        // (cl.hasOption("ssl") ? "https://" : "http://")
+        // + username + ":" + password + "@" + hostname + ":"
+        // + port + uri;
+        String encoded =
+                Base64.encodeBase64String((username + ":" + password)
+                        .getBytes());
+        Properties props = new Properties();
+        props.put("Authorization", "Basic " + encoded);
+        String response = null;
+        String errmsg = null;
+        try {
+            response = Utils.getUrl(new URL(url), props, timeout * 1000);
+        } catch (MalformedURLException e) {
+            // e.printStackTrace();
+            log.info("Bad plugin URL configuration : " + e.getMessage());
+            errmsg = e.getMessage();
+        } catch (IOException e) {
+            log.info("Plugin execution failed : " + e.getMessage(), e);
+            errmsg = e.getMessage();
+        }
+
+        if (response == null) {
+            return new ReturnValue(Status.WARNING, errmsg);
+        }
+
+        return analyseStatus(response, warning, critical);
+    }
+
+    /**
+     * Parse xml data and return status.
+     *
+     * @param xml
+     *            The XML to be analyzed
+     * @param warning
+     *            The warning range
+     * @param critical
+     *            The critical range
+     * @return ReturnValue The reesult
+     */
+    private ReturnValue analyseStatus(final String xml, final String warning,
+            final String critical) {
+        StringBuffer buff = new StringBuffer();
+
+        String[] warn = warning != null ? warning.split(",") : null;
+        String[] crit = critical != null ? critical.split(",") : null;
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = null;
-		try {
-			builder = factory.newDocumentBuilder();
-		} catch (ParserConfigurationException e) {
-			e.printStackTrace();
-		}
+        try {
+            builder = factory.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
         InputSource is = new InputSource(new StringReader(xml));
         try {
-        	Document doc = builder.parse(is);
-			XPathFactory xPathfactory = XPathFactory.newInstance();
-			XPath xpath = xPathfactory.newXPath();			
-			Element root = (Element)xpath.compile("//status").evaluate(doc.getDocumentElement(), XPathConstants.NODE);
-			Element memory = (Element)xpath.compile("//status/jvm/memory").evaluate(doc.getDocumentElement(), XPathConstants.NODE);
-			
-			int free = Integer.parseInt(memory.getAttribute("free"));
-			int total = Integer.parseInt(memory.getAttribute("total"));
-			int max = Integer.parseInt(memory.getAttribute("max"));
-			int available = free + max - total;
-			
-			if (critical != null && critical.length() == 2 && !"".equals(crit[1])){
-				int criticalMemory = getValue(crit[1], max) * 1024 * 1024;
-				if (available < criticalMemory){
-					return new ReturnValue(Status.CRITICAL, "Free memory critical: " + available + " MB available");
-				}
-			}	
-			
-			if (warning != null && warn.length == 2 && !"".equals(warn[1])){
-				int warnMemory = getValue(warn[1], max) * 1024 * 1024;
-				if (available < warnMemory){
-					return new ReturnValue(Status.WARNING, "Free memory low: " + available + " MB available");
-				}
-			}
-			
-			int memUse = (max - available);
-			buff.append("JVM memory use " + Utils.formatSize(memUse) + " ");
-			buff.append("Free: " + Utils.formatSize(free) + ", Total: " + Utils.formatSize(total) + ", Max: " + Utils.formatSize(max) + " ");
-			NodeList connectors = root.getElementsByTagName("connector");
-			int threadWarn = -1;
-			int threadCrit =-1;
-			if (warn != null && !"".equals(warn[0])) {
-				threadWarn = Integer.parseInt(warn[0]);
-			}
-			if (crit != null && !"".equals(crit[0])) {
-				threadCrit = Integer.parseInt(crit[0]);
-			}
-			
-			for (int i = 0; i < connectors.getLength(); i++){
-				Element connector = (Element)connectors.item(i);
-				String connectorName = connector.getAttribute("name");
-				Element threadInfo = (Element)connector.getElementsByTagName("threadInfo").item(0);
-				int maxThreads = Integer.parseInt(threadInfo.getAttribute("maxThreads"));
-				int currentThreadCount = Integer.parseInt(threadInfo.getAttribute("currentThreadCount"));
-				int currentThreadsBusy = Integer.parseInt(threadInfo.getAttribute("currentThreadsBusy"));
-				int threadsAvailable = maxThreads - currentThreadsBusy;
-				if (threadsAvailable <= threadCrit){
-					return new ReturnValue(Status.CRITICAL, "Free " + connectorName + " threads: " + threadsAvailable);
-				}
-				if (threadsAvailable <= threadWarn){
-					return new ReturnValue(Status.WARNING, "Free " + connectorName + " threads: " + threadsAvailable);
-				}
-				
-				buff.append(connectorName + " - thread count: " + currentThreadCount + ", current threads busy: " + currentThreadsBusy + ", max threads: " + maxThreads + " ");
-			}
-			
-		} catch (XPathExpressionException e) {
-			e.printStackTrace();
-			
-		} catch (SAXException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-			
-		return new ReturnValue(Status.OK, buff.toString());
-	}
+            Document doc = builder.parse(is);
+            XPathFactory xPathfactory = XPathFactory.newInstance();
+            XPath xpath = xPathfactory.newXPath();
+            Element root =
+                    (Element) xpath.compile("//status").evaluate(
+                            doc.getDocumentElement(), XPathConstants.NODE);
+            Element memory =
+                    (Element) xpath.compile("//status/jvm/memory").evaluate(
+                            doc.getDocumentElement(), XPathConstants.NODE);
 
-	/**
-	 * Extract numeric value, even if it's a %
-	 * 
-	 * @param value
-	 * @param factor
-	 * @return
-	 * int
-	 */
-	private int getValue(String value, int factor){
-		int val = 0;
-		if (value != null){
-			if (value.contains("%")){
-				val = factor * Integer.parseInt(value.replace("%", "")) * 100;
-			}else{
-				val = Integer.parseInt(value);
-			}
-		}
-		return val;
-	}
-	
+            int free = Integer.parseInt(memory.getAttribute("free"));
+            int total = Integer.parseInt(memory.getAttribute("total"));
+            int max = Integer.parseInt(memory.getAttribute("max"));
+            int available = free + max - total;
+
+            if (critical != null && critical.length() == 2
+                    && !"".equals(crit[1])) {
+                int criticalMemory = getValue(crit[1], max) * 1024 * 1024;
+                if (available < criticalMemory) {
+                    return new ReturnValue(Status.CRITICAL,
+                            "Free memory critical: " + available
+                                    + " MB available");
+                }
+            }
+
+            if (warning != null && warn.length == 2 && !"".equals(warn[1])) {
+                int warnMemory = getValue(warn[1], max) * 1024 * 1024;
+                if (available < warnMemory) {
+                    return new ReturnValue(Status.WARNING, "Free memory low: "
+                            + available + " MB available");
+                }
+            }
+
+            int memUse = (max - available);
+            buff.append("JVM memory use " + Utils.formatSize(memUse) + " ");
+            buff.append("Free: " + Utils.formatSize(free) + ", Total: "
+                    + Utils.formatSize(total) + ", Max: "
+                    + Utils.formatSize(max) + " ");
+            NodeList connectors = root.getElementsByTagName("connector");
+            int threadWarn = -1;
+            int threadCrit = -1;
+            if (warn != null && !"".equals(warn[0])) {
+                threadWarn = Integer.parseInt(warn[0]);
+            }
+            if (crit != null && !"".equals(crit[0])) {
+                threadCrit = Integer.parseInt(crit[0]);
+            }
+
+            for (int i = 0; i < connectors.getLength(); i++) {
+                Element connector = (Element) connectors.item(i);
+                String connectorName = connector.getAttribute("name");
+                Element threadInfo =
+                        (Element) connector.getElementsByTagName("threadInfo")
+                                .item(0);
+                int maxThreads =
+                        Integer.parseInt(threadInfo.getAttribute("maxThreads"));
+                int currentThreadCount =
+                        Integer.parseInt(threadInfo
+                                .getAttribute("currentThreadCount"));
+                int currentThreadsBusy =
+                        Integer.parseInt(threadInfo
+                                .getAttribute("currentThreadsBusy"));
+                int threadsAvailable = maxThreads - currentThreadsBusy;
+                if (threadsAvailable <= threadCrit) {
+                    return new ReturnValue(Status.CRITICAL, "Free "
+                            + connectorName + " threads: " + threadsAvailable);
+                }
+                if (threadsAvailable <= threadWarn) {
+                    return new ReturnValue(Status.WARNING, "Free "
+                            + connectorName + " threads: " + threadsAvailable);
+                }
+
+                buff.append(connectorName + " - thread count: "
+                        + currentThreadCount + ", current threads busy: "
+                        + currentThreadsBusy + ", max threads: " + maxThreads
+                        + " ");
+            }
+
+        } catch (XPathExpressionException e) {
+            e.printStackTrace();
+
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new ReturnValue(Status.OK, buff.toString());
+    }
+
+    /**
+     * Extract numeric value, even if it's a %.
+     *
+     * @param value
+     *            The value
+     * @param factor
+     *            The factor
+     * @return int The numeric value
+     */
+    private int getValue(final String value, final int factor) {
+        int val = 0;
+        if (value != null) {
+            if (value.contains("%")) {
+                val = factor * Integer.parseInt(value.replace("%", "")) * 100;
+            } else {
+                val = Integer.parseInt(value);
+            }
+        }
+        return val;
+    }
+
 }
---
+++ jnrpe-parent/jnrpe-plugins/src/test/java/it/jnrpe/plugins/test/CheckTomcatTest.java
@@ -0,0 +1,163 @@
+package it.jnrpe.plugins.test;
+
+import it.jnrpe.ReturnValue;
+import it.jnrpe.Status;
+import it.jnrpe.client.JNRPEClient;
+import it.jnrpe.commands.CommandDefinition;
+import it.jnrpe.commands.CommandOption;
+import it.jnrpe.commands.CommandRepository;
+import it.jnrpe.plugins.PluginDefinition;
+import it.jnrpe.utils.PluginRepositoryUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import org.apache.commons.io.FileUtils;
+import org.codehaus.cargo.container.ContainerType;
+import org.codehaus.cargo.container.InstalledLocalContainer;
+import org.codehaus.cargo.container.configuration.ConfigurationType;
+import org.codehaus.cargo.container.configuration.LocalConfiguration;
+import org.codehaus.cargo.container.installer.ZipURLInstaller;
+import org.codehaus.cargo.generic.DefaultContainerFactory;
+import org.codehaus.cargo.generic.configuration.DefaultConfigurationFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * Test for the checktomcat plugin.
+ *
+ * @author Massimiliano Ziccardi
+ */
+@Test
+public class CheckTomcatTest implements Constants {
+
+    /**
+     * The tomcat download url.
+     */
+    private static final String TOMCAT_DOWNLOAD_URL =
+            "http://apache.fastbull.org/tomcat/tomcat-7/v7.0.40"
+                    + "/bin/apache-tomcat-7.0.40.zip";
+
+    /**
+     * The tomcat container.
+     */
+    private InstalledLocalContainer container = null;
+
+    /**
+     * Writes the tomcat-users.xml giving all the privileges to the user
+     * tomcat/tomcat.
+     *
+     * @param f
+     *            The destination file
+     * @throws IOException
+     *             -
+     */
+    private void enableTomcatConsole(final File f) throws IOException {
+        String xml =
+                "<?xml version='1.0' encoding='utf-8'?>"
+                        + "<tomcat-users>"
+                        + "<role rolename=\"manager-gui\"/>"
+                        + "<role rolename=\"manager-script\"/>"
+                        + "<role rolename=\"manager-jmx\"/>"
+                        + "<role rolename=\"manager-status\"/>"
+                        + "<user username=\"tomcat\" password=\"tomcat\" "
+                        + "roles=\"manager-gui,manager-script,manager-jmx,"
+                        + "manager-status\"/>"
+                        + "</tomcat-users>";
+
+        FileUtils.writeStringToFile(f, xml);
+    }
+
+    /**
+     * Starts tomcat.
+     */
+    @BeforeClass
+    public final void setup() {
+        try {
+            ClassLoader cl = CheckFilePluginTest.class.getClassLoader();
+
+            PluginDefinition checkTomcat =
+                    PluginRepositoryUtil.parseXmlPluginDefinition(cl,
+                            cl.getResourceAsStream("check_tomcat_plugin.xml"));
+
+            SetupTest.getPluginRepository().addPluginDefinition(checkTomcat);
+
+            File tomcatDir = new File("./target/tomcat");
+
+            tomcatDir.mkdirs();
+
+            ZipURLInstaller installer =
+                    new ZipURLInstaller(new URL(TOMCAT_DOWNLOAD_URL));
+            installer.setExtractDir(tomcatDir.getAbsolutePath());
+            installer.install();
+
+            DefaultConfigurationFactory factory =
+                    new DefaultConfigurationFactory();
+            factory.registerConfiguration("tomcat7xJnrpeTest",
+                    ContainerType.INSTALLED,
+                    ConfigurationType.STANDALONE, Tomcat7xConf.class);
+
+            LocalConfiguration configuration =
+                    (LocalConfiguration) factory
+                            .createConfiguration(
+                                    "tomcat7xJnrpeTest",
+                                    ContainerType.INSTALLED,
+                                    ConfigurationType.STANDALONE);
+
+            container =
+                    (InstalledLocalContainer) new DefaultContainerFactory()
+                            .createContainer(
+                                    "tomcat7x", ContainerType.INSTALLED,
+                                    configuration);
+
+            container.setHome(installer.getHome());
+
+            ((Tomcat7xConf) configuration).realConfigure(container);
+            enableTomcatConsole(new File(new File(configuration.getHome(),
+                    "conf"), "tomcat-users.xml"));
+
+            container.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Executes the test.
+     * @throws Exception -
+     */
+    @Test
+    public final void checkTomcatOK() throws Exception {
+        CommandRepository cr = SetupTest.getCommandRepository();
+
+        cr.addCommandDefinition(new CommandDefinition("CHECK_TOMCAT",
+                "CHECK_TOMCAT")
+                .addArgument(new CommandOption("hostname", "$ARG1$"))
+                .addArgument(new CommandOption("port", "$ARG2$"))
+                .addArgument(new CommandOption("username", "$ARG3$"))
+                .addArgument(new CommandOption("password", "$ARG4$"))
+                );
+
+        JNRPEClient client = new JNRPEClient(BIND_ADDRESS, JNRPE_PORT, false);
+        ReturnValue ret =
+                client.sendCommand("CHECK_TOMCAT",
+                        "127.0.0.1", "8080", "tomcat", "tomcat");
+
+        Assert.assertEquals(ret.getStatus(), Status.OK, ret.getMessage());
+    }
+
+    /**
+     * Stop tomcat.
+     */
+    @AfterClass
+    public final void tearDown() {
+        if (container != null) {
+            container.stop();
+        }
+    }
+
+}
