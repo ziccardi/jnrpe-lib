5657e448270f3de1fc4345def41b351db68d6dc4
ziccardi <massimiliano.ziccardi@gmail.com>
2013-10-28 13:30:15 +0100

    Added SLF4J to LogService binding

---
+++ jnrpe-parent/jnrpe-osgi-core/src/main/java/org/slf4j/impl/OSGiLogger.java
@@ -0,0 +1,175 @@
+package org.slf4j.impl;
+
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.log.LogService;
+import org.slf4j.helpers.MarkerIgnoringBase;
+import org.slf4j.helpers.MessageFormatter;
+
+public class OSGiLogger extends MarkerIgnoringBase
+{
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Check the availability of the OSGI logging service, and use it is available.
+     * Does nothing otherwise.
+     * @param level
+     * @param message
+     * @param t
+     */
+    final private void internalLog(final int level, final Object message, final Throwable t)
+    {
+        LogService logservice = OSGILogFactory.getLogService();
+        ServiceReference serviceref = OSGILogFactory.getServiceReference();
+
+        if (logservice != null)
+        {
+            try {
+                if (t != null)
+                    logservice.log(serviceref, level, message.toString(), t);
+                else
+                    logservice.log(serviceref, level, message.toString());
+            } catch (Exception exc)
+            {
+                // Service may have become invalid, just ignore any error
+                // until the log service reference is updated by the
+                // log factory.
+            }
+        }
+    }
+
+    public boolean isTraceEnabled() {
+        return true;
+    }
+    public void trace(final String msg) {
+        internalLog(LogService.LOG_DEBUG, msg, null);
+    }
+
+    public void trace(final String format, final Object arg) {
+        String msgStr = MessageFormatter.format(format, arg).getMessage();
+        internalLog(LogService.LOG_DEBUG, msgStr, null);
+    }
+
+    public void trace(final String format, final Object arg1, final Object arg2) {
+        String msgStr = MessageFormatter.format(format, arg1, arg2).getMessage();
+        internalLog(LogService.LOG_DEBUG, msgStr, null);
+    }
+
+
+    public void trace(final String format, final Object ... argArray) {
+        String msgStr = MessageFormatter.arrayFormat(format, argArray).getMessage();
+        internalLog(LogService.LOG_DEBUG, msgStr, null);
+    }
+
+    public void trace(final String msg, final Throwable t) {
+        internalLog(LogService.LOG_DEBUG, msg, t);
+    }
+
+
+
+    public boolean isDebugEnabled() {
+        return true;
+    }
+    public void debug(final String msg) {
+        internalLog(LogService.LOG_DEBUG, msg, null);
+    }
+    public void debug(final String format, final Object arg) {
+        String msgStr = MessageFormatter.format(format, arg).getMessage();
+        internalLog(LogService.LOG_DEBUG, msgStr, null);
+    }
+
+    public void debug(final String format, final Object arg1, final Object arg2) {
+        String msgStr = MessageFormatter.format(format, arg1, arg2).getMessage();
+        internalLog(LogService.LOG_DEBUG, msgStr, null);
+    }
+
+    public void debug(final String format, final Object ... argArray) {
+        String msgStr = MessageFormatter.arrayFormat(format, argArray).getMessage();
+        internalLog(LogService.LOG_DEBUG, msgStr, null);
+    }
+
+    public void debug(final String msg, final Throwable t) {
+        internalLog(LogService.LOG_DEBUG, msg, t);
+    }
+
+    public boolean isInfoEnabled() {
+        return true;
+    }
+
+    public void info(final String msg) {
+        internalLog(LogService.LOG_INFO, msg, null);
+    }
+
+    public void info(final String format, final Object arg) {
+        String msgStr = MessageFormatter.format(format, arg).getMessage();
+        internalLog(LogService.LOG_INFO, msgStr, null);
+    }
+    public void info(final String format, final Object arg1, final Object arg2) {
+        String msgStr = MessageFormatter.format(format, arg1, arg2).getMessage();
+        internalLog(LogService.LOG_INFO, msgStr, null);
+    }
+
+    public void info(final String format, final Object ... argArray) {
+        String msgStr = MessageFormatter.arrayFormat(format, argArray).getMessage();
+        internalLog(LogService.LOG_INFO, msgStr, null);
+    }
+
+    public void info(final String msg, final Throwable t) {
+        internalLog(LogService.LOG_INFO, msg, t);
+    }
+
+    public boolean isWarnEnabled() {
+        return true;
+    }
+    public void warn(final String msg) {
+        internalLog(LogService.LOG_WARNING, msg, null);
+    }
+
+    public void warn(final String format, final Object arg) {
+        String msgStr = MessageFormatter.format(format, arg).getMessage();
+        internalLog(LogService.LOG_WARNING, msgStr, null);
+    }
+
+    public void warn(final String format, final Object arg1, final Object arg2) {
+        String msgStr = MessageFormatter.format(format, arg1, arg2).getMessage();
+        internalLog(LogService.LOG_WARNING, msgStr, null);
+    }
+
+    public void warn(final String format, final Object ... argArray) {
+        String msgStr = MessageFormatter.arrayFormat(format, argArray).getMessage();
+        internalLog(LogService.LOG_WARNING, msgStr, null);
+    }
+
+    public void warn(final String msg, final Throwable t) {
+        internalLog(LogService.LOG_WARNING, msg, t);
+    }
+
+    public boolean isErrorEnabled() {
+        return true;
+    }
+
+    public void error(final String msg) {
+        internalLog(LogService.LOG_ERROR, msg, null);
+    }
+
+    public void error(final String format, final Object arg) {
+        String msgStr = MessageFormatter.format(format, arg).getMessage();
+        internalLog(LogService.LOG_ERROR, msgStr, null);
+    }
+
+    public void error(final String format, final Object arg1, final Object arg2) {
+        String msgStr = MessageFormatter.format(format, arg1, arg2).getMessage();
+        internalLog(LogService.LOG_ERROR, msgStr, null);
+    }
+    public void error(final String format, final Object ... argArray) {
+        String msgStr = MessageFormatter.arrayFormat(format, argArray).getMessage();
+        internalLog(LogService.LOG_ERROR, msgStr, null);
+    }
+
+    public void error(final String msg, final Throwable t) {
+        internalLog(LogService.LOG_ERROR, msg, t);
+    }
+
+}
